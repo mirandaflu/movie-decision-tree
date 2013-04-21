8fa083e2da990edd3e3d7ece6b7510b6cc8bc72a
mirandaflu <miranda.fluh@gmail.com>
2013-04-21 18:09:29 -0400

    formatted data, updated test script

--- aima-python/dtree_usinglib.py
+++ aima-python/dtree_usinglib.py
@@ -2,16 +2,30 @@ from learning import *
 
 learner = DecisionTreeLearner()
 
-movies = DataSet(name='movies_formatted', #for display, csv file matches
-                 target='like_movie', #attr trying to predict
-                 
-                 #list of attributes to predict on
-                 attrnames="like_movie like_director like_actors dislike_actors series_seen_previous trailer positive_rating recommended access remake_seen_original blank1 blank2 subscribe_online blank3 blank4 theater_close like_theater blank5 blank6 with_whom blank7 blank8 cost opening_weekend")
+filename = ''
 
-learner.train(movies)
+while 1:
+    if filename != '':
+        print '\n(type exit to exit)'
+        
+    filename = raw_input("which movie survey?")
+    
+    if filename == 'exit':
+        break
+    
+    movies = DataSet(name='movies'+filename, #for display, csv file matches
+                     target='like_movie', #attr trying to predict
+                     
+                     #list of attributes to predict on
+                     attrnames="like_movie like_director like_actors dislike_actors series_seen_previous trailer positive_rating recommended access remake_seen_original blank1 blank2 subscribe_online blank3 blank4 theater_close like_theater blank5 blank6 with_whom blank7 blank8 cost opening_weekend")
 
-verbose = 2 # 0=none, 1=only negative outcomes, 2=all
+    learner.train(movies)
 
-test(learner, movies, None, verbose)
+    verbose = 1 # 0=none, 1=only negative outcomes, 2=all
 
-print 'done'
+    print 'accuracy: '
+    print test(learner, movies, None, verbose)
+
+    print 'done'
+
+#2A gives list index out of range error
