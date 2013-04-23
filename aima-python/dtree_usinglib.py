8c6f6a124e7b1a2de9165d5647c81bf18ad7a111
Plopmaster <jgb304@nyu.edu>
2013-04-22 21:51:25 -0400

    Displays the decision tree

--- aima-python/dtree_usinglib.py
+++ aima-python/dtree_usinglib.py
@@ -20,7 +20,8 @@ while 1:
                      attrnames="like_movie like_director like_actors dislike_actors series_seen_previous trailer positive_rating recommended access remake_seen_original blank1 blank2 subscribe_online blank3 blank4 theater_close like_theater blank5 blank6 with_whom blank7 blank8 cost opening_weekend")
 
     learner.train(movies)
-
+    learner.dt.display()
+	
     verbose = 1 # 0=none, 1=only negative outcomes, 2=all
 
     print 'accuracy: '
