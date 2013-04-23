fc56275679c6825c8efffd6413aa576ea586b494
mirandaflu <miranda.fluh@gmail.com>
2013-04-22 23:38:46 -0400

    data matches up (removed blank# attribute names)

--- aima-python/dtree_usinglib.py
+++ aima-python/dtree_usinglib.py
@@ -17,7 +17,7 @@ while 1:
                      target='like_movie', #attr trying to predict
                      
                      #list of attributes to predict on
-                     attrnames="like_movie like_director like_actors dislike_actors series_seen_previous trailer positive_rating recommended access remake_seen_original blank1 blank2 subscribe_online blank3 blank4 theater_close like_theater blank5 blank6 with_whom blank7 blank8 cost opening_weekend")
+                     attrnames="like_movie like_director like_actors dislike_actors series_seen_previous trailer positive_rating recommended access remake_seen_original subscribe_online theater_close like_theater with_whom cost opening_weekend")
 
     learner.train(movies)
     learner.dt.display()
